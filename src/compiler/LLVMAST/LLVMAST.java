89969a1250f6dba6725cefa5f5194a97a2c34de6
Michal Filipiuk <mkfilipiuk@gmail.com>
2021-01-09 15:18:28 +0100

    Added malloc.

--- src/compiler/LLVMAST/LLVMAST.java
+++ src/compiler/LLVMAST/LLVMAST.java
@@ -661,6 +661,7 @@ public class LLVMAST {
         stringBuilder.append("declare i32 @readInt_function()\n");
         stringBuilder.append("declare i8* @readString_function()\n");
         stringBuilder.append("declare void @error_function()\n");
+        stringBuilder.append("declare i8* @malloc(i32)\n");
 
         stringBuilder.append(LLVMContext.generateStrings());
 
